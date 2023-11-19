ec96be77fec3cf25ee73e82778cbd680607f736c
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-11-19 21:10:47 +0100

    Update Fibonacci_Secuence.java
     The current state of the code has an execution time complexity of 2 to the power of n.

--- Fibonacci_Secuence.java
+++ Fibonacci_Secuence.java
@@ -1,8 +1,13 @@
 class Source {
   public static init fib(int n) {
-  
-    
+    if (n == 0 || n == 1) {
+      return n;
+    }
+    return fib(n - 1) + fib(n - 2);
   }
-
+public static void run() {
+  // This function behaves as 'main()' for the 'run' command
+  // you may sandbox in this function, but should not remove it
+}
   
 }
