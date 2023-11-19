0b1081e4df28f1a3f8754f4e1fddebd67d622dd4
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-11-19 21:47:55 +0100

    Update Fibonacci_Secuence.java
    Now the program has two functions. The main one with the HashMap library overloading the main method, and the second is an auxiliary function also with hashmap.

--- Fibonacci_Secuence.java
+++ Fibonacci_Secuence.java
@@ -1,5 +1,13 @@
+import java.util.HashMap;
+
 class Source {
-  public static init fib(int n) {
+
+  public static int fib(int n) {
+    fib(n, new HashMap<>())
+    
+  }
+  //Main function with supercharged method
+  public static init fib(int n, HashMap<Integer, Integer> memo) {
     if (n == 0 || n == 1) {
       return n;
     }
