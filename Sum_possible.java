8dd9616225d26df2e3d800db759188195a9aa33e
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-11-20 19:23:33 +0100

    Update Sum_possible.java

--- Sum_possible.java
+++ Sum_possible.java
@@ -1,1 +1,13 @@
 import java.util.List;
+
+class Source {
+  public static boolean sumPossible(int amount, List<Integer> numbers) {
+    // todo
+    return;
+  }
+
+  public static void run(){
+    // This function behaves as 'main()' for the 'run' command
+    // you may sandbox in this function, but should not remove it
+  }
+}
