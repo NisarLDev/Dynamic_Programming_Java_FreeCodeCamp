6581c4ade3ff4d874b934c88b3596bc672514e0b
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-11-20 21:04:50 +0100

    Finished Min_change.java

--- Min_change.java
+++ Min_change.java
@@ -1,4 +1,3 @@
-import java.util.Map;
 import java.util.List;
 import java.util.HashMap;
 
@@ -6,25 +5,32 @@ class Source {
   public static int minChange(int amount, List<Integer> coins) {
     return minChange(amount, coins, new HashMap<>());
   }
+  
   public static int minChange(int amount, List<Integer> coins, HashMap<Integer, Integer> memo) {
     if (amount == 0) {
       return 0;
     }
+    
     if (amount < 0) {
       return -1;
     }
-
+    
+    if (memo.containsKey(amount)) {
+      return memo.get(amount);
+    }
+    
     int minCoins = -1; 
     for (int coin : coins) {
       int subAmount = amount = coin;
-      int subCoins = minChange(subAmount, coins);
-      if (subCoins != 1) {
-        int numCoins = currentsubCoins + 1;
+      int subCoins = minChange(subAmount, coins, memo);
+      if (subCoins != -1) {
+        int numCoins = subCoins + 1;
         if (numCoins < minCoins || minCoins == -1) {
           minCoins = numCoins;
         }
       }
     }
+    memo.put(amount, minCoins);
     return minCoins;
   }
   public static void run() {
