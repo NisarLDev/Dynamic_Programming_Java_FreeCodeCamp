3715756ae5e2dc36fd66b9782af9cb24d929ff4b
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-11-20 20:41:47 +0100

    Update Min_change.java

--- Min_change.java
+++ Min_change.java
@@ -1,6 +1,6 @@
 import java.util.Map;
 import java.util.List;
-import java.util.HashMap;
+import java.util.HashSet;
 
 class Source {
   public static int minChange(int amount, List<Integer> coins) {
