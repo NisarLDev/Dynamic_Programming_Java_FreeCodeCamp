977ff3d76da5265bdfbc8c9122b4f774c33d797f
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-11-20 19:46:34 +0100

    Update Sum_possible.java
    Added method HashMap.

--- Sum_possible.java
+++ Sum_possible.java
@@ -1,7 +1,8 @@
 import java.util.List;
+import java.util.HashMap;
 
 class Source {
-  public static boolean sumPossible(int amount, List<Integer> numbers) {
+  public static boolean sumPossible(int amount, List<Integer> numbers, HashMap<>) {
     if (amount == 0) {
       return true;
     }
