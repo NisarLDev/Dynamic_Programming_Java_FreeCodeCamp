dc80f4b7f99bd9f4de52471e1f559eefa2ea1522
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-11-20 20:32:32 +0100

    Update Min_change.java

--- Min_change.java
+++ Min_change.java
@@ -1,1 +1,14 @@
+import java.util.List;
 
+class Source {
+  public static int minChange(int amount, List<Integer> coins) {
+    if (amount == 0) {
+      return 0;
+    }
+  }
+}
+
+public static void run() {
+  // This function behaves as 'main()' for the 'run' command
+  // you may sandbox in this function, but should not remove it
+}
