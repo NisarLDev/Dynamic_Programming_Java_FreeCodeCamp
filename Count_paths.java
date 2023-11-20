aadc78d8e72a341b6f634db45995fb83c548226a
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-11-20 21:20:49 +0100

    Update Count_paths.java

--- Count_paths.java
+++ Count_paths.java
@@ -5,7 +5,7 @@ class Source {
   public static int countPaths(List<List<Strings>> grid){
     return countPaths(0, 0, grid, new HashMap<>());
   }
-  public static int countPaths(int r, int c, List<List<Strings>> grid, HashMap<>){
+  public static int countPaths(int r, int c, List<List<Strings>> grid, HashMap<List<Integer, Integer> memo){
     if (r == grid.size() || c == grid.get(0).size()) {
       return 0;
     }
