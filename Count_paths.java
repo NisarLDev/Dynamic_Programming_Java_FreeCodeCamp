2f5f4a6ff74411b9b663dea5c944d94c7c885fd3
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-11-20 21:10:32 +0100

    Update Count_paths.java

--- Count_paths.java
+++ Count_paths.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 class Source {
-  public static int countPaths(){
-    
+  public static int countPaths(List<List<Strings>> grid){
+    return countPaths(0, 0, grid);
   }
 }
